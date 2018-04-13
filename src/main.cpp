5ee238da188ddbe7f15b5c21fc34392c46b7b710
Dcbaas <Dcbaas@github.com>
2018-04-13 00:16:47 -0400

    unfinished algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
-
+#include "Vector.h"
+#include "Edge.h"
+#include <vector>
+#include <string>
+std::vector<Edge> prims_algorithm(std::vector<Vertex> graph, Vertex & startPoint);
 
 int main(int argc, char** argv){
 
 return 0;
+}
+
+std::vector<Edge> prims_algorithm(std::vector<Vertex> graph, Vertex & startPoint{
+  std::vector<Vertex> coveredVertices;  
+  std::vector<Edge> spanningEdges;
+  
+  while(coveredVertices.size() != graph.size()){
+    Edge lowest();
+ 
+    for(auto vertex_it = graph.begin(); vertex_it != graph.end(); ++vertex_it){
+      
+    }
+  }
+  for  
 }
